2e9750d1efeb4d978fe9fa23c91feb1ed6b650a0
Tomás Migone <tomas@balena.io>
2020-10-05 17:27:42 -0300

    service: fix install script
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- service/install.sh
+++ service/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-REPO_URL="https://github.com/balena-io-playground/balena-ide"
+REPO_URL="https://raw.githubusercontent.com/balena-io-playground/balena-ide"
 
 # Remount fs as read/write
 mount -o remount,rw /
@@ -9,10 +9,10 @@ mount -o remount,rw /
 mkdir /etc/balena-ide
 mkdir /usr/lib/balena-ide
 
-curl "$REPO_URL/tree/master/service/balena-ide-healthcheck" --output /usr/lib/balena-ide/balena-ide-healthcheck
-curl "$REPO_URL/tree/master/service/balena-ide.service" --output /lib/systemd/system/balena-ide.service
-curl "$REPO_URL/tree/master/service/start-balena-ide" --output /usr/bin/start-balena-ide
-curl "$REPO_URL/tree/master/service/ide.conf" --output /etc/balena-ide/ide.conf
+curl --silent "$REPO_URL/master/service/balena-ide-healthcheck" --output /usr/lib/balena-ide/balena-ide-healthcheck
+curl --silent "$REPO_URL/master/service/balena-ide.service" --output /lib/systemd/system/balena-ide.service
+curl --silent "$REPO_URL/master/service/start-balena-ide" --output /usr/bin/start-balena-ide
+curl --silent "$REPO_URL/master/service/ide.conf" --output /etc/balena-ide/ide.conf
 
 chmod +x /usr/bin/start-balena-ide
 chmod +x /usr/lib/balena-ide/balena-ide-healthcheck
